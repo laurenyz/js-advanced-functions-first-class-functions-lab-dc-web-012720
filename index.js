ba05dd1bd94bde7a07cdc1fa33f9be6cd91924d5
Lauren Yu <laurenkyu@gmail.com>
2020-03-28 21:41:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ const fareDoubler= createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
 
-function selectDifferentDrivers(drivers, function) {
-  
+function selectDifferentDrivers(drivers, whatDrivers) {
+  return whatDrivers(drivers)
 }
